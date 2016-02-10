5455060c13663cd40b4439ec8047fa9bc966a27a
ToBaer94 <ToBaer94@users.noreply.github.com>
2016-02-10 14:11:28 +0100

    Added missing .tsx files

---
+++ sky objects.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sky objects" tilewidth="129" tileheight="106" tilecount="9" columns="0">
+ <tile id="0">
+  <image width="128" height="71" source="world/cloud1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="129" height="71" source="world/cloud3.png"/>
+ </tile>
+ <tile id="2">
+  <image width="129" height="71" source="world/cloud2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="70" height="70" source="world/plant.png"/>
+ </tile>
+ <tile id="4">
+  <image width="70" height="70" source="world/plantPurple.png"/>
+ </tile>
+ <tile id="5">
+  <image width="70" height="70" source="world/rock.png"/>
+ </tile>
+ <tile id="6">
+  <image width="70" height="70" source="world/bush.png"/>
+ </tile>
+ <tile id="7">
+  <image width="70" height="70" source="world/cactus.png"/>
+ </tile>
+ <tile id="8">
+  <image width="48" height="106" source="world/hill_small.png"/>
+ </tile>
+</tileset>
